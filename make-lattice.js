5e9bfd2ae52f701998b31394b0afaae15383c669
justmeet0924 <93417919+justmeet0924@users.noreply.github.com>
2023-12-23 12:22:20 +0900

    Update make-lattice.js

--- make-lattice.js
+++ make-lattice.js
@@ -1,11 +1,15 @@
-function make_lattice(num) {
+function make_lattice(num,array) {
+	
+	let w = window.innerWidth;
+	let h = window.innerHeight;
+	let s = min(w,h);
   for (let i = 1; i < num; i++) {
     for (let j = 1; j < num; j++) {
       let x = (width / num) * i;
       let y = (height / num) * j;
-      let r = s / (lattice_num)*(2/5);
+      let r = s / (num)*(2/5);
 
-      lattice.push(new BounceBall(x, y, r, m, g, k, s));
+      array.push(new Ball(x, y, r));
     }
   }
 }
